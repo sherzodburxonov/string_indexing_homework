07078d62f097fba12c3016970eac6e057e883e5f
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-20 08:45:58 +0500

    05

--- Index05.py
+++ Index05.py
@@ -6,4 +6,22 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    x1=s[0]
+    x2=s[1]
+    x3=s[2]
+    x4=s[3]
+    x5=s[4]
+    s=0
+    if x1.isdigit():
+        s=s+1
+    if x2.isdigit():
+        s=s+1
+    if x3.isdigit():
+        s=s+1
+    if x4.isdigit():
+        s=s+1
+    if x5.isdigit():
+        s=s+1
+    return s
+    
+print(main("sf478"))
