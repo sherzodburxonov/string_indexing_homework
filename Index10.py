31759c87f448f7f99bd3259a5954179fc7220c89
burxonovsherzod2105@gmail.com <burxonovsherzod2105@gmail.com>
2023-04-20 08:57:43 +0500

    10

--- Index10.py
+++ Index10.py
@@ -6,4 +6,20 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    x1=s[0]
+    x2=s[1]
+    x3=s[2]
+    x4=s[3]
+    x5=s[4]
+    if x1.isdigit():
+        s=int(x1)
+    if x2.isdigit():
+        s=s+int(x2)
+    if x3.isdigit():
+        s=s+int(x3)
+    if x4.isdigit():
+        s=s+int(x4)
+    if x5.isdigit():
+        s=s+int(x5)
+    return s
+print(main("12k45"))
